e5121fd23c7445afb26f2001006f5db14f12e63f
Jack Wong <jackwongau@hotmail.com>
2024-02-02 21:32:40 +1100

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(name: "SLAnimation",
+                      platforms: [.iOS(.v12)],
+                      products: [.library(name: "SLAnimation",
+                                          targets: ["SLAnimation"])],
+                      targets: [.target(name: "SLAnimation",
+                                        path: "SLAnimation",
+                                        publicHeadersPath: "")])
